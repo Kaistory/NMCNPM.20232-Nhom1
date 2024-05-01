f15313e8e4e6b2888d9a32fe92e4ef1d50ae321f
Kaistory <111366297+Kaistory@users.noreply.github.com>
2024-05-02 00:25:40 +0700

    da_co_tai_khoan

---
+++ backup/chung_cu_bluemoon_nhan_khau.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: chung_cu_bluemoon
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `nhan_khau`
+--
+
+DROP TABLE IF EXISTS `nhan_khau`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nhan_khau` (
+  `ID` int NOT NULL,
+  `CCCD` int NOT NULL,
+  `TEN` varchar(45) COLLATE utf8mb3_bin NOT NULL,
+  `SDT` int DEFAULT NULL,
+  `EMAIL` varchar(60) COLLATE utf8mb3_bin DEFAULT NULL,
+  `NGAY_SINH` date NOT NULL,
+  `DIA_CHI` varchar(120) COLLATE utf8mb3_bin DEFAULT NULL,
+  `TRANG_THAI` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL,
+  `GIOI_TINH` int DEFAULT NULL,
+  `QUOC_TICH` varchar(45) COLLATE utf8mb3_bin DEFAULT NULL,
+  `MQH` int DEFAULT NULL,
+  `ID_CHU_HO` int DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nhan_khau`
+--
+
+LOCK TABLES `nhan_khau` WRITE;
+/*!40000 ALTER TABLE `nhan_khau` DISABLE KEYS */;
+INSERT INTO `nhan_khau` VALUES (1,251,'duong khai',375,'k4224','2014-02-02','fda','dinh cu',NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `nhan_khau` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-02  0:23:41
--- chung_cu_bluemoon_1.sql
+++ chung_cu_bluemoon_1.sql
@@ -202,6 +202,29 @@ LOCK TABLES `pt_gui` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `tai_khoan`
+--
+
+DROP TABLE IF EXISTS `tai_khoan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tai_khoan` (
+  `TEN_TAI_KHOAN` varchar(45) COLLATE utf8mb3_bin NOT NULL,
+  `MAT_KHAU` varchar(45) COLLATE utf8mb3_bin DEFAULT NULL,
+  PRIMARY KEY (`TEN_TAI_KHOAN`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tai_khoan`
+--
+
+LOCK TABLES `tai_khoan` WRITE;
+/*!40000 ALTER TABLE `tai_khoan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tai_khoan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `thu_tien`
 --
 
@@ -234,4 +257,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-02  0:14:17
+-- Dump completed on 2024-05-02  0:16:02
